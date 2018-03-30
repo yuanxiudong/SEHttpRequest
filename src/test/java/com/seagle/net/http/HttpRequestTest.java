837d33b12d340714ca1be45cde7e8b987f99e21c
袁秀东 <xiudong.yuan@midea.com>
2018-03-30 20:49:57 +0800

    modify test unit

--- src/test/java/com/seagle/net/http/HttpRequestTest.java
+++ src/test/java/com/seagle/net/http/HttpRequestTest.java
@@ -11,9 +11,8 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
 
 /**
  * Http request test.
@@ -26,18 +25,8 @@ public class HttpRequestTest {
         HttpRequest httpRequest = new HttpRequest("http://ip.taobao.com/service/getIpInfo.php?ip=210.21.220.218", handler);
         httpRequest.setReadTimeout(10000, TimeUnit.MILLISECONDS);
         httpRequest.setConnectTimeout(10000, TimeUnit.MILLISECONDS);
-        httpRequest.getHeader().setHeader("Content-Type","text/plain");
+        httpRequest.getHeader().setHeader("Content-Type", "text/plain");
         HttpResponse response = httpRequest.doGet(null, null).getResponse();
-
-        httpRequest.doGet(null, new HttpCallback() {
-            @Override
-            public void onRequestComplete(HttpRequest httpRequest, HttpResponse httpResponse) {
-                int code = httpResponse.getCode();
-                String message = httpResponse.getMessage();
-                HttpHeader header = httpResponse.getHeader();
-            }
-        });
-
         int code = response.getCode();
         String message = response.getMessage();
         HttpHeader header = response.getHeader();
@@ -64,7 +53,7 @@ public class HttpRequestTest {
         HttpTextResponseHandler handler = new HttpTextResponseHandler();
         HttpFormRequest httpRequest = new HttpFormRequest("http://ip.taobao.com/service/getIpInfo.php", handler);
         httpRequest.addParam("ip", "210.21.220.218");
-        httpRequest.addParam(new HttpFormRequest.FormParam("ip","210.21.220.218"));
+        httpRequest.addParam(new HttpFormRequest.FormParam("ip", "210.21.220.218"));
         httpRequest.setReadTimeout(10, TimeUnit.MINUTES);
         httpRequest.setConnectTimeout(10, TimeUnit.MINUTES);
         httpRequest.doGet(null, null);
@@ -123,43 +112,45 @@ public class HttpRequestTest {
         final CountDownLatch latch = new CountDownLatch(1);
         String fileUrl = "http://img.zcool.cn/community/01e44d5711c84f6ac72513437994cb.jpg@2o.jpg";
         File file = new File("D:/1234.png");
-        file.createNewFile();
-        HttpFileDownloadResponseHandler handler = new HttpFileDownloadResponseHandler(file);
-        final HttpFormRequest httpRequest = new HttpFormRequest(fileUrl, handler);
-        httpRequest.setReadTimeout(10, TimeUnit.MINUTES);
-        httpRequest.setConnectTimeout(10, TimeUnit.MINUTES);
-        httpRequest.doGet(null, new HttpCallback() {
-            @Override
-            public void onRequestComplete(HttpRequest request,HttpResponse httpResponse) {
-                latch.countDown();
-                HttpResponse response = httpRequest.getResponse();
-                int code = response.getCode();
-                String message = response.getMessage();
-                HttpHeader header = response.getHeader();
-                Object body = response.getBody();
-                System.out.println(String.format("Response[code:%d  message:%s]", code, message));
-                if (response.getCode() == 200) {
-                    File bodyFile = (File) body;
-                    System.out.println(header.getVersion());
-                    for (String key : header.getAllHeaders().keySet()) {
-                        System.out.println(String.format("%s:%s", key, header.getHeader(key)));
+        if (file.createNewFile()) {
+            HttpFileDownloadResponseHandler handler = new HttpFileDownloadResponseHandler(file);
+            final HttpFormRequest httpRequest = new HttpFormRequest(fileUrl, handler);
+            httpRequest.setReadTimeout(10, TimeUnit.MINUTES);
+            httpRequest.setConnectTimeout(10, TimeUnit.MINUTES);
+            httpRequest.doGet(null, new HttpCallback() {
+                @Override
+                public void onRequestComplete(HttpRequest request, HttpResponse httpResponse) {
+                    latch.countDown();
+                    HttpResponse response = httpRequest.getResponse();
+                    int code = response.getCode();
+                    String message = response.getMessage();
+                    HttpHeader header = response.getHeader();
+                    Object body = response.getBody();
+                    System.out.println(String.format("Response[code:%d  message:%s]", code, message));
+                    if (response.getCode() == 200) {
+                        File bodyFile = (File) body;
+                        System.out.println(header.getVersion());
+                        for (String key : header.getAllHeaders().keySet()) {
+                            System.out.println(String.format("%s:%s", key, header.getHeader(key)));
+                        }
+                        System.out.println();
+                        System.out.println(bodyFile.exists());
+                    } else {
+                        Throwable throwable = (Throwable) body;
+                        if (throwable != null) {
+                            throwable.printStackTrace();
+                        }
+                        fail("Request failed: (" + response.getCode() + ", " + response.getMessage() + ")");
                     }
-                    System.out.println();
-                    System.out.println(bodyFile.exists());
-                } else {
-                    Throwable throwable = (Throwable) body;
-                    if (throwable != null) {
-                        throwable.printStackTrace();
-                    }
-                    fail("Request failed: (" + response.getCode() + ", " + response.getMessage() + ")");
                 }
+            });
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } else {
+            fail();
         }
-
     }
 }
